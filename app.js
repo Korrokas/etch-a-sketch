b3db5c9b8a7c749bdec6cc82d7259d866e4426b7
Brandon <kounse@gmail.com>
2022-09-07 19:30:33 -0400

    Completed Etch-a-Sketch project. Added button to reset the grid of squares, the logic works through each case and null, and each square changes colors!

--- app.js
+++ app.js
@@ -5,21 +5,39 @@ containerDiv.setAttribute('id', 'containerDiv');
 body.appendChild(containerDiv);
 
 
-//Generate the 16x16 grid of Divs
-for (let i = 0; i < 256; i ++) {
-    const gridDiv = document.createElement('div');
-    gridDiv.setAttribute('class', 'gridDiv');
-    containerDiv.appendChild(gridDiv);
+//Function to generate a grid
+function generateGrid(num) {
+    for (let i = 0; i < Math.pow(num, 2); i ++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.setAttribute('class', 'gridDiv');
+        containerDiv.appendChild(gridDiv);
+    }
+    containerDiv.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    containerDiv.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 }
+
 
 //Create an event listener for hover when entering div
-const boxesInGrid = document.querySelectorAll('.gridDiv');
+function createEventsForGrid() {
+    const boxesInGrid = document.querySelectorAll('.gridDiv');
 
-for (let i = 0; i < boxesInGrid.length; i++) {
-    boxesInGrid[i].addEventListener('mouseenter', changeColor);
+    for (let i = 0; i < boxesInGrid.length; i++) {
+        boxesInGrid[i].addEventListener('mouseenter', changeColor);
+    }
 }
 
-//Define function for changing colors
+
+//Function for removing old grid
+function removeOldGrid() {
+    const removeGrid = document.querySelectorAll('.gridDiv');
+
+    for (let i = 0; i < removeGrid.length; i++) {
+        removeGrid[i].remove();
+    }
+}
+
+
+//Function for changing colors
 function changeColor() {
     const colorValues = randomColor();
     this.style['background-color'] = `rgb(${colorValues[0]}, ${colorValues[1]}, ${colorValues[2]})`;
@@ -34,3 +52,26 @@ function randomColor() {
 }
 
 //Button that prompts user for # of squares maximum 100, generate in same total space
+const newGridBtn = document.querySelector('#newGridBtn');
+newGridBtn.addEventListener('click', newGrid);
+
+function newGrid() {
+    const numberOfSquares = prompt("Please enter how many squares for new Etch-a-Sketch: (100 or less)");
+    if (numberOfSquares === null) {
+        return;
+    }
+    else if (numberOfSquares > 100 || numberOfSquares < 1) {
+        alert("invalid number, please resubmit: ");
+        newGrid();
+    }
+    else {
+        removeOldGrid();
+        generateGrid(numberOfSquares);
+        createEventsForGrid();
+    }
+}
+
+
+
+generateGrid(16);
+createEventsForGrid();
